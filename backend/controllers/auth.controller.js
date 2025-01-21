8b371372806c55bc2b83e232723d6f820b8fae6c
Soorya dev <sooryadevanikkat@gmail.com>
2025-01-21 17:57:59 +0530

    Initial commit: Twitter Clone setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = (req, res) => {
+    res.json({
+        data: "signup hitted"
+    })
+}
+export const login = (req, res) => {
+    res.json({
+        data: "login hitted"
+    })
+}   
+export const logout = (req, res) => {
+    res.json({
+        data: "logout hitted"
+    })
+}

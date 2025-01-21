ab73181f26c9e65e5b64ced8c58b7fd0a9ca948a
Soorya dev <sooryadevanikkat@gmail.com>
2025-01-21 21:02:50 +0530

    Auth completed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import authRoutes from './routes/auth.routes.js' 
 import dotenv from 'dotenv'
 import connectMongoDB from './db/connectMongoDB.js'
+import cookieParser from 'cookie-parser'
 
 
 
@@ -11,6 +12,11 @@ dotenv.config()
 
 const app = express()
 const PORT=process.env.PORT || 5000
+
+app.use(express.json()) //to get data from body
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 
 
 app.use("/api/auth",authRoutes)
